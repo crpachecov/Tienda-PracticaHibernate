edeea1019747f9aa94ede6a32e7e77c549d9fa53
crpachecov <crpacheco@unicesar.edu.co>
2023-05-06 11:03:47 -0500

    Inicio practica Hibernate

---
+++ src/main/java/com/latam/alura/tienda/App.java
@@ -0,0 +1,13 @@
+package com.latam.alura.tienda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
